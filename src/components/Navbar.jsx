2f264b3137885ff845435fcd7a0d0300bf2d9313
shorya20 <78030563+shorya20@users.noreply.github.com>
2022-07-27 20:23:21 +0400

    email link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,7 +70,7 @@ const Navbar = () => {
           </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration 300 bg-gray-600'>
-            <a className="flex justify-between items-center w-full text-gray-300" href="https://www.linkedin.com/in/shorya-sinha-1389a81b6/">
+            <a className="flex justify-between items-center w-full text-gray-300" onClick={()=>window.location = "mailto:shoryaoct20@gmail.com"}>
             Email <HiOutlineMail size={25} />
           </a>
           </li>
